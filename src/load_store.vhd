8d7f98fcd93482b837dab5f88c05fb2deabed661
Rubem Kalebe <rubemkalebe@hotmail.com>
2016-03-08 22:16:41 -0300

    Load/store sketch

---
+++ src/load_store.vhd
@@ -0,0 +1,14 @@
+-- Short description...
+-- Version: 03.08.2016.
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity load_store is
+end load_store;
+
+architecture load_store of load_store is
+
+begin
+  
+end load_store;
